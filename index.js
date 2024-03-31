a8ea990a755c2556fd59ca31562d8c7e3c239cff
Mia012022 <wz1103202107@gmail.com>
2024-03-31 15:03:21 +0800

    scrollUpdate

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
+//滑鼠移過去 圖片翻轉效果
 function moveUp2(element) {
-    element.style.transform = "rotate(-10deg)";//往 左 轉10度
+    //往 左 轉10度
+    element.style.transform = "rotate(-10deg)";
 }
 function moveDown2(element) {
-    element.style.transform = "translateY(0px)";//返回原本位置
-
+    //返回原本位置
+    element.style.transform = "translateY(0px)";//
 }
 function moveUp(element) {
-    element.style.transform = "rotate(10deg)";//往 右 轉10度
+    //往 右 轉10度
+    element.style.transform = "rotate(10deg)";
 }
 function moveDown(element) {
-    element.style.transform = "translateY(0px)";//返回原本位置
-
+    //返回原本位置
+    element.style.transform = "translateY(0px)";
 }
+//scroll滑上滑下 更改背景顏色
 const introduce = document.querySelector("article");
-window.addEventListener('wheel', (event) => {           //wheel滑鼠滾輪
-    const direction = event.deltaY > 0 ? 'down' : 'up';//deltaY滾輪時往下滑為 正 , 往上滑為 負
-
+//wheel滑鼠滾輪
+window.addEventListener('wheel', (event) => {
+    //deltaY滾輪時往下滑為 正 , 往上滑為 負
+    const direction = event.deltaY > 0 ? 'down' : 'up';
     // 根据滚动方向改变背景颜色
     if (direction === 'down') {
         introduce.style.backgroundColor = 'grey';
@@ -24,7 +29,7 @@ window.addEventListener('wheel', (event) => {           //wheel滑鼠滾輪
     }
 }
 );
-
+//載入網頁 動畫效果
 window.addEventListener('load', function () {
     const image = document.getElementById('image');
     const logoimg = document.getElementById('logoimg');
@@ -36,6 +41,7 @@ window.addEventListener('load', function () {
 
     logoimg.style.opacity = '1';
     logoimg.style.transform = 'translateX(0)';
+    //因為className有很多個，所以每個都要呼叫，用迴圈一個一個叫出來
     for (let i = 0; i < dropBtn.length; i++) {
         dropBtn[i].style.opacity = '1';
         dropBtn[i].style.transform = 'translateY(0)';
@@ -46,14 +52,28 @@ window.addEventListener('load', function () {
     }
     // 可以根据需要添加其他定位效果，比如旋转、缩放等
 });
+//卷軸拉下  最新消息 文字圖片載入
 window.addEventListener('scroll', function () {
     const newsTitle = document.getElementById('newsTitle');
+    const newsthree = document.getElementsByClassName('newsthree');
     const scrollHeight = window.scrollY;
-    const appearPosition = 550;
-    const moveDistance = 200;
+    const appearPosition = 550;//newsTitle最新消息
+    const appearPosition2 =700;//newsthree消息列表
+    const appearPosition3 =1200;//newsthree消息列表
+
     if (scrollHeight > appearPosition) {
         const opacity = (scrollHeight - appearPosition) / 200;
+        const opacity2 =(scrollHeight - appearPosition2)/200;
+        const opacity3 =(scrollHeight - appearPosition3)/200;
         newsTitle.style.opacity = opacity > 1 ? 1 : opacity;
-        newsTitle.style.bottom = `${-moveDistance + (scrollHeight - appearPosition)}px`;
-    } 
+        for (let i = 0; i < newsthree.length; i++) {
+            newsthree[i].style.opacity = opacity2 > 1 ? 1 : opacity2;
+        }
+
+    } else {
+        newsTitle.style.opacity = 0; // 滚动位置未达到出现位置时，设置透明度为0
+        for (let i = 0; i < newsthree.length; i++) {
+            newsthree[i].style.opacity = 0; // 滚动位置未达到出现位置时，设置透明度为0
+        }
+    }
 });
